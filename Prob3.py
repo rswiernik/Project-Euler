e07fe936182e50cb510c15f223a5da6a3bc2be9d
reed <reed@ashkelon.csh.rit.edu>
2014-03-02 17:17:19 -0500

    Added problem 8

--- Prob3.py
+++ Prob3.py
@@ -1,29 +1,21 @@
-# Work in Progress
+#!/usr/bin/python
 
-#x = 600851475143
-x = 13195
-p = []
+import math
+import time
 
-for n in range(2, x/2):
-	for i in p:
-		if(n%i==0):
-			break
-	else:
-		#print "added " + str(n)
-		p.append(n)
-	#n += 1
+def factorPrimes(num):
+	highestFactor = 0
+	for cur_num in range(2,int(math.sqrt(num)+1)):
+		isprime = True
+		for x in range(2, int(math.sqrt(cur_num) + 1)):
+			if cur_num % x == 0: 
+				isprime = False
+				break
+		if isprime:
+			if( isprime and num % cur_num == 0):
+				highestFactor = cur_num
+	print highestFactor
 
-highest = 0
-for i in p:
-	#print "Prime: " + str(i)
-	try:
-		index = p.index(x%i)
-		if(i%1==0 and (x%i)%1==0):
-			#print "Updated highest > " + str(i)
-			highest = i
-	except ValueError:
-		break
-		#print str(i) + " is not a factor"
-			
-
-print highest
+start = time.time()
+factorPrimes(600851475143)
+print "Time to calculate: %s seconds" % ((time.time()-start))
